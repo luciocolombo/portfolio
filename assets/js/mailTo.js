0cb63e0f6e6474c1ba8299fe531830943c7fb398
lucio <colombolucio@hotmail.com>
2021-03-10 12:33:35 -0300

    v

--- assets/js/mailTo.js
+++
@@ -1,9 +0,0 @@
-function sendMail() {
-    var link = "mailto:colombolucio@hotmail.com"
-             + "?cc=myCCaddress@example.com"
-             + "&subject=" + encodeURIComponent("This is my subject")
-             + "&body=" + encodeURIComponent(document.getElementById('myText').value)
-    ;
-    
-    window.location.href = link;
-}
